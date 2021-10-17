583062771e129070f91f14ad37aecd11759b31e2
Diego Nunez <diegonunez.dev@gmail.com>
2021-10-16 22:37:23 -0400

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ListItem from './components/ListItem';
+
+import { SAMPLE_DATA } from './assets/data/sampleData';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleWrapper}>
+        <Text style={styles.largeTitle}>Markets</Text>
+      </View>
+      <View style={styles.divider} />
+    
+      <ListItem 
+        name={SAMPLE_DATA[0].name}
+        symbol={SAMPLE_DATA[0].symbol}
+        currentPrice={SAMPLE_DATA[0].current_price}
+        priceChangePercentage7d={SAMPLE_DATA[0].price_change_percentage_7d_in_currency}
+        logoUrl={SAMPLE_DATA[0].image}
+      />
     </View>
   );
 }
@@ -15,7 +29,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  largeTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  titleWrapper: {
+    marginTop: 80,
+    paddingHorizontal: 16,
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#A9ABB1',
+    marginHorizontal: 16,
+    marginTop: 16,
+  }
 });
---
+++ components/ListItem.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+const ListItem = ({name, symbol, currentPrice, priceChangePercentage7d, logoUrl}: 
+    {name: string, symbol: string, currentPrice: number, priceChangePercentage7d: number, logoUrl: string}) => {
+    return (
+        <TouchableOpacity>
+            <View style={styles.itemWrapper}>
+                {/* Left Side View */}
+                <View style={styles.leftWrapper}>
+                    <Image source={{uri: logoUrl}} style={styles.image} />
+                    <View style={styles.titleWrapper}>
+                        <Text style={styles.title}>{name}</Text>
+                        <Text style={styles.subtitle}>{symbol}</Text>
+                    </View>
+                </View>
+                {/* Right Side View */}
+                <View style={styles.rightWrapper}>
+                    <Text style={styles.title}>{currentPrice}</Text>
+                    <Text style={[styles.subtitle, {color: 'red'}]}>{priceChangePercentage7d}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    itemWrapper: {
+        paddingHorizontal: 16,
+        marginTop: 24,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    leftWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    }, 
+    rightWrapper: {
+        alignItems: 'flex-end',
+    },
+    image: {
+        height: 48,
+        width: 48,
+    },
+    titleWrapper: {
+        marginLeft: 8,
+    },
+    title: {
+        fontSize: 18,
+    },
+    subtitle: {
+        marginTop: 4,
+        fontSize: 14,
+        color: "#A9ABB1",
+    },
+})
+
+export default ListItem;
+
